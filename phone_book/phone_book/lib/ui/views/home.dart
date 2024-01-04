ef3b2ae697907a495e8eb38746925d97d3142fa0
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2024-01-04 17:10:58 +0300

    version 1 final

--- phone_book/phone_book/lib/ui/views/home.dart
+++ phone_book/phone_book/lib/ui/views/home.dart
@@ -11,19 +11,120 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  bool isSearch = false;
+
+  Future<void> search(String searchWord) async {
+    print("$searchWord");
+  }
+
+  Future<List<Persons>> uploadPersons() async {
+    var p1 = Persons(id: 1, name: "zehra", number: "1234");
+    var p2 = Persons(id: 2, name: "akın", number: "12345");
+    var p3 = Persons(id: 3, name: "ali", number: "12346");
+    var p4 = Persons(id: 4, name: "ayşe", number: "12347");
+    var persons = [p1, p2, p3, p4];
+    return persons;
+  }
+
+  Future<void> deletePerson(int id) async {
+    print("$id");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Kişiler"),
+        title: isSearch
+            ? TextField(
+                decoration: InputDecoration(hintText: "Ara"),
+                onChanged: (searchResult) {
+                  search(searchResult);
+                },
+              )
+            : Text("Kişiler"),
+        actions: [
+          isSearch
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isSearch = false;
+                    });
+                  },
+                  icon: Icon(Icons.clear))
+              : IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isSearch = true;
+                    });
+                  },
+                  icon: Icon(Icons.search),
+                ),
+        ],
       ),
-      body: ElevatedButton(
-          onPressed: () {
-            var person = Persons(id: 1, name: "zehra", number: "123");
-            Navigator.push(context, MaterialPageRoute(builder: (context) => DetailView(person: person)));
-          },
-          child: Text("detay deneme")),
+      body: FutureBuilder(
+          future: uploadPersons(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var personList = snapshot.data;
+              return ListView.builder(
+                  itemCount: personList!.length,
+                  itemBuilder: (context, index) {
+                    var person = personList[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 48.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          print("${person.name} seçildi");
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => DetailView(person: person)));
+                        },
+                        child: Card(
+                          child: SizedBox(
+                            height: 100,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      person.name.toUpperCase(),
+                                      style: TextStyle(fontSize: 28),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      person.number,
+                                      style: TextStyle(fontSize: 18),
+                                    ),
+                                  ],
+                                ),
+                                IconButton(
+                                    onPressed: () {
+                                      ScaffoldMessenger.of(context).showSnackBar(
+                                        SnackBar(
+                                          content: Text("${person.name} silinsin mi?"),
+                                          action: SnackBarAction(
+                                            label: "Silinsin",
+                                            onPressed: () {
+                                              deletePerson(person.id);
+                                            },
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    icon: Icon(Icons.clear))
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+            } else {
+              return Text("Kişi Listeniz Boş");
+            }
+          }),
       floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
         onPressed: () {
           Navigator.push(context, MaterialPageRoute(builder: (context) => const AddView())).then(
             (value) => print("geri dönüldü"), //then sayfaya geri dönüldüğünde yapılacak işleri düzenler
