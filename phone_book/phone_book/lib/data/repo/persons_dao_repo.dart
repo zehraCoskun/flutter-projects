14dbb2f69c9523b239945be63ba90e3122db5499
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2024-01-06 12:40:03 +0300

    sqlite added

--- phone_book/phone_book/lib/data/repo/persons_dao_repo.dart
+++ phone_book/phone_book/lib/data/repo/persons_dao_repo.dart
@@ -1,29 +1,29 @@
 import 'package:phone_book/data/entity/persons.dart';
+import 'package:phone_book/data/sqlite/database_util.dart';
 
 class PersonsDaoRepo {
-
   Future<void> saved(String name, String phone) async {
     print("$name - $phone");
   }
 
-    Future<void> updated(int id, String name, String phone) async {
+  Future<void> updated(int id, String name, String phone) async {
     print("$name - $phone");
   }
 
-    Future<void> deletePerson(int id) async {
+  Future<void> deletePerson(int id) async {
     print("$id");
   }
 
-    Future<List<Persons>> uploadPersons() async {
-    var p1 = Persons(id: 1, name: "zehra", number: "1234");
-    var p2 = Persons(id: 2, name: "akın", number: "12345");
-    var p3 = Persons(id: 3, name: "ali", number: "12346");
-    var p4 = Persons(id: 4, name: "ayşe", number: "12347");
-    var persons = [p1, p2, p3, p4];
-    return persons;
+  Future<List<Persons>> uploadPersons() async {
+    var db = await DatabaseUtil.databaseAccess();
+    List<Map<String, dynamic>> maps = await db.rawQuery("SELECT * FROM PERSON");
+    return List.generate(maps.length, (index) {
+      var line = maps[index];
+      return Persons(id: line["person_id"], name: line["person_name"], number: line["person_number"]);
+    });
   }
 
-    Future<List<Persons>>search(String searchWord) async {
+  Future<List<Persons>> search(String searchWord) async {
     var p1 = Persons(id: 1, name: "zehra", number: "1234");
     var persons = [p1];
     return persons;
