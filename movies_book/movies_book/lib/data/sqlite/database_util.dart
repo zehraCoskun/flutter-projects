36b6ee22802716d06f96635415d60f38951ec02c
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2024-01-08 00:03:30 +0300

    database updated

--- movies_book/movies_book/lib/data/sqlite/database_util.dart
+++ movies_book/movies_book/lib/data/sqlite/database_util.dart
@@ -4,17 +4,17 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseUtil {
-  static const String databaseName = "moviesbook.db";
+  static const String databaseName = "movies.sqlite";
   static Future<Database> databaseAccess() async {
     String databasePath = join(await getDatabasesPath(), databaseName);
-    // if (await databaseExists(databasePath)) {
-    //   print("veri tabanı mevcut, kopyalamaya gerek yok");
-    // } else {
-    ByteData data = await rootBundle.load("database/$databaseName");
-    List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-    await File(databasePath).writeAsBytes(bytes, flush: true);
-    print("veri tabanı kopyalandı");
-    // }
+    if (await databaseExists(databasePath)) {
+      print("veri tabanı mevcut, kopyalamaya gerek yok");
+    } else {
+      ByteData data = await rootBundle.load("database/$databaseName");
+      List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      await File(databasePath).writeAsBytes(bytes, flush: true);
+      print("veri tabanı kopyalandı");
+    }
     return openDatabase(databasePath);
   }
 }
