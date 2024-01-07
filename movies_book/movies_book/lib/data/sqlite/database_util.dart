d9ac36a98465e05a35f671342043d59b0387cee4
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2024-01-07 17:18:44 +0300

    database added

--- movies_book/movies_book/lib/data/sqlite/database_util.dart
+++ movies_book/movies_book/lib/data/sqlite/database_util.dart
@@ -1,21 +1,20 @@
-// import 'dart:io';
-// import 'package:flutter/services.dart';
-// import 'package:path/path.dart';
-// import 'package:sqflite/sqflite.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
-// class DatabaseUtil {
-//   static const String databaseName = "phonebook.db";
-
-//   static Future<Database> databaseAccess() async {
-//     String databasePath = join(await getDatabasesPath(), databaseName);
-//     if (await databaseExists(databasePath)) {
-//       print("veri tabanı mevcut, kopyalamaya gerek yok");
-//     } else {
-//       ByteData data = await rootBundle.load("/database/$databaseName");
-//       List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-//       await File(databasePath).writeAsBytes(bytes, flush: true);
-//       print("veri tabanı kopyalandı");
-//     }
-//     return openDatabase(databasePath);
-//   }
-// }
+class DatabaseUtil {
+  static const String databaseName = "moviesbook.db";
+  static Future<Database> databaseAccess() async {
+    String databasePath = join(await getDatabasesPath(), databaseName);
+    if (await databaseExists(databasePath)) {
+      print("veri tabanı mevcut, kopyalamaya gerek yok");
+    } else {
+      ByteData data = await rootBundle.load("/database/$databaseName");
+      List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      await File(databasePath).writeAsBytes(bytes, flush: true);
+      print("veri tabanı kopyalandı");
+    }
+    return openDatabase(databasePath);
+  }
+}
